5dd69b84e5133a906fed339b304b250c94c3f172
arendon7 <102555888+arendon7@users.noreply.github.com>
2026-08-23 12:00:56 -0500

    W78: export deterministic PAPER execution model

--- src/autotrade/brokers/__init__.py
+++ src/autotrade/brokers/__init__.py
@@ -1,4 +1,20 @@
 from .base import BrokerExecution, ExecutionBroker
 from .paper import PaperBroker
+from .paper_execution import (
+    DeterministicPaperExecutionBroker,
+    PaperExecutionConfig,
+    PaperExecutionConflict,
+    PaperExecutionError,
+    PaperExecutionMarketError,
+)
 
-__all__ = ["BrokerExecution", "ExecutionBroker", "PaperBroker"]
+__all__ = [
+    "BrokerExecution",
+    "DeterministicPaperExecutionBroker",
+    "ExecutionBroker",
+    "PaperBroker",
+    "PaperExecutionConfig",
+    "PaperExecutionConflict",
+    "PaperExecutionError",
+    "PaperExecutionMarketError",
+]
